b75fdf27dba379b901a39ae3e671b3c22b3c3bc7
Adam MacDonald <am1macdonald@gmail.com>
2023-05-08 07:20:56 -0400

    stupid

--- cats_effect_00/src/main/scala/Main.scala
+++ cats_effect_00/src/main/scala/Main.scala
@@ -1,5 +1,19 @@
 import cats.effect.{IO, IOApp}
+import scala.concurrent.duration._
 
-object HelloWorld extends IOApp.Simple {
-  val run = IO.println("Hello, World!")
+// obviously this isn't actually the problem definition, but it's kinda fun
+object StupidFizzBuzz extends IOApp.Simple {
+  val run =
+    for {
+      ctr <- IO.ref(0)
+
+      wait = IO.sleep(1.second)
+      poll = wait *> ctr.get
+
+      _ <- poll.flatMap(IO.println(_)).foreverM.start
+      _ <- poll.map(_ % 3 == 0).ifM(IO.println("fizz"), IO.unit).foreverM.start
+      _ <- poll.map(_ % 5 == 0).ifM(IO.println("buzz"), IO.unit).foreverM.start
+
+      _ <- (wait *> ctr.update(_ + 1)).foreverM.void
+    } yield ()
 }
